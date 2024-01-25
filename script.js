1f81dd8dce3c66e793963d8b7ea793e05b1ab397
Jorge-Salas7 <jorge.salas@lasallistas.org.mx>
2024-01-25 12:35:19 -0600

    acabando la pagina

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //funcion que pone estilo a lo seleccionado y quita la anterior
 function seleccionar(link) {
-    
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //en el responsive hacemos que el menu se guarde al seleccionar una opcion
+    var x = document.getElementById("nav");
+    x.className = "";
 }
 
 //funcion que muestra el menu responsive
@@ -10,5 +20,21 @@ function responsiveMenu() {
         x.className = "responsive";
     } else {
         x.className = "";
+    }
+}
+
+//animacion del scrolling para aplicar la animacion de la barra
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+//aplica la animacion de la barra de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight = skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("c#").classList.add("barra-progreso2");
+        document.getElementById("unity").classList.add("barra-progreso3");
     }
 }
